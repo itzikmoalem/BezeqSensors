b86e95c98bc85096eb2e617bc240e9542840313c
itzikmoalem <65330570+itzikmoalem@users.noreply.github.com>
2022-12-22 17:00:01 +0200

    finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { homePageComponent } from './components/homePage/homePage.component';
+import { sensorComponent } from './components/sensor/sensor.component';
+
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import { ListfilterPipe } from './pipes/listfilter.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    homePageComponent,
+    sensorComponent, 
+    ListfilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatGridListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
